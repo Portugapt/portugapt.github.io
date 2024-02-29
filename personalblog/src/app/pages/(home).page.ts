b7b5f72ae1c23d25d74655322ce1aacaae7e5cc9
Portugapt <monteiro.joao.ps@gmail.com>
2024-02-29 00:23:09 +0000

    router link to blog

--- personalblog/src/app/pages/(home).page.ts
+++ personalblog/src/app/pages/(home).page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import { AnalogWelcomeComponent } from './analog-welcome.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'personalblog-home',
   standalone: true,
-  imports: [AnalogWelcomeComponent],
+  imports: [AnalogWelcomeComponent, RouterLink],
   template: `
+     <a [routerLink]="['/blog']">Blog</a>
      <personalblog-analog-welcome/>
   `,
 })
