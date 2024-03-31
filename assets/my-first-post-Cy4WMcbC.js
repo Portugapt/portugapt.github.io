7addd01f627c566e3d53aff761f15f09ae8c335f
Portugapt <monteiro.joao.ps@gmail.com>
2024-03-31 22:54:53 +0000

    Update from https://github.com/Portugapt/i-am-online/commit/b0014509484a2c3e47ab26d3e0c075a686cfd7c4

---
+++ assets/my-first-post-Cy4WMcbC.js
@@ -0,0 +1,20 @@
+const n=`---
+title: My First Post
+slug: my-first-post
+---
+
+Hello World
+
+one two three
+
+\`\`\`python
+print('hello world')
+\`\`\`
+
+<div class='bg-white-200'>
+    <h1>hello world</h1>
+    <p>one two three</p>
+    <code>
+        print('hello world')
+    </code>
+</div>`;export{n as default};
--- assets/my-first-post-nsD074vD.js
+++
@@ -1,9 +0,0 @@
-const t=`---
-title: My First Post
-slug: my-first-post
----
-
-Hello World
-
-one two three
-`;export{t as default};
